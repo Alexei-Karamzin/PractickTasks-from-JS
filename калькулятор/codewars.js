d88c5a8d802840941be6a14f893daf8348f227d6
Alexei-Karamzin <95247963+Alexei-Karamzin@users.noreply.github.com>
2021-12-19 00:20:16 +0300

    Add files via upload

--- калькулятор/codewars.js
+++ калькулятор/codewars.js
@@ -1,20 +1,15 @@
-let input1 = document.querySelector('#inputSq');
-let input2 = document.querySelector('#inputPr');
-let button1 = document.querySelector('#buttonSq');
-let button2 = document.querySelector('#buttonPr');
+let input1 = document.querySelector('#input1');
+let input2 = document.querySelector('#input2');
+let result = document.querySelector('#result');
+
+let button1 = document.querySelector('#button1');
+let button2 = document.querySelector('#button2');
 
 button1.addEventListener('click', function(){
-  input1.value = Number(input1.value) * Number(input1.value);
+  console.log(result.value);
+  result.value = Number(input1.value) * Number(input2.value); 
 });
-
 button2.addEventListener('click', function(){
-  input2.value = Number(input2.value) * 4
-});
-
-input1.addEventListener('click', function(){
-  input1.value = '';
-});
-
-input2.addEventListener('click', function(){
-  input2.value = '';
+  console.log(result.value);
+  result.value = (Number(input1.value) * 2) + (Number(input2.value) * 2); 
 });
