9c27aa933c12539d2a000a07468c79ebb3ffc577
Alexei-Karamzin <95247963+Alexei-Karamzin@users.noreply.github.com>
2021-12-19 14:22:53 +0300

    Add files via upload

--- калькулятор окружности/codewars.js
+++ калькулятор окружности/codewars.js
@@ -1,21 +1,17 @@
 let button1 = document.querySelector('#button1');
 let input1 = document.querySelector('#input1');
-
 let button2 = document.querySelector('#button2');
 let input2 = document.querySelector('#input2');
-
-button1.addEventListener('click', function(){
-  input1.value = Number(input1.value) * 3,14;
-});
-
-button2.addEventListener('click', function(){
-  input2.value = 2 * Number(input2.value) * 3,14;
-});
 
 input1.addEventListener('click', function(){
   input1.value = '';
 });
-
 input2.addEventListener('click', function(){
   input2.value = '';
+});
+button1.addEventListener('click', function(){
+  input1.value = 3.14 * Number(input1.value) * Number(input1.value);
+});
+button2.addEventListener('click', function(){
+  input2.value = 3.14 * Number(input1.value) * 2;
 });
